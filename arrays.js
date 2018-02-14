fea2107cdc3600f58bcf0f4ff82ed3d2a7827915
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-14 03:55:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,6 @@ var chocolateBars = [
 function addElementToBeginningOfArray(array, element) {
   return [element, ... array]
 } 
-];
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return [array.unshift(element)]
